a3cb54ab876fc946c6db1458d702bbe97f63d3da
Alex Ellis <AlexEllis2@gmail.com>
2016-12-22 13:28:30 +0000

    Static binary

--- watchdog/build.sh
+++ watchdog/build.sh
@@ -2,6 +2,5 @@
 
 docker build -t watchdog:latest . -f Dockerfile.build
 docker create --name buildoutput watchdog:latest
-docker cp buildoutput:/go/src/fwatchdog/fwatchdog ./
+docker cp buildoutput:/go/src/app/app ./fwatchdog
 docker rm buildoutput
-
