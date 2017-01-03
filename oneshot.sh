df8e506ee6a19c17b1c324a320d9bc72e9816e49
Alex <alexellis2@gmail.com>
2017-01-03 23:40:10 +0000

    Run in background

--- oneshot.sh
+++ oneshot.sh
@@ -8,5 +8,5 @@
  docker service rm catservice ; docker service create --network=functions --name catservice catservice
  cd ..
  cd gateway
- docker build -t server . ;docker rm -f server; docker run -v /var/run/docker.sock:/var/run/docker.sock --name server -p 8080:8080 --network=functions server
+ docker build -t server . ;docker rm -f server; docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name server -p 8080:8080 --network=functions server
 
