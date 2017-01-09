ef5c83f7e582756e00e5bb456d2cd1d6783071ac
Alex Ellis <alexellis2@gmail.com>
2017-01-09 17:51:03 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ cp ./fwatchdog ../sample-functions/catservice/
 
 docker build --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
     -t alexellis2/faas-catservice .
-docker service rm catservice ; docker service create --network=functions --name catservice alexellis2/faas-catservice
+ #docker service rm catservice ; docker service create --network=functions --name catservice alexellis2/faas-catservice
 
 cd ..
 
 cd gateway
 ./build.sh
-docker rm -f server; docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name server -p 8080:8080 --network=functions alexellis2/faas-gateway
+# docker rm -f server; docker run -d -v /var/run/docker.sock:/var/run/docker.sock --name server -p 8080:8080 --network=functions alexellis2/faas-gateway
