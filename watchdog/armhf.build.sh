6091878c5aebe2e6c8e7916fce108d3d947d6cab
Alex Ellis <alexellis2@gmail.com>
2017-01-24 19:38:06 +0000

    Initial work for ARMHF

--- watchdog/armhf.build.sh
+++ watchdog/armhf.build.sh
@@ -3,8 +3,11 @@
 # Below makes use of "builder pattern" so that binary is extracted separate
 # from the golang runtime/SDK
 
+echo Building alexellis2/faas-watchdog:build-armhf
+
 docker build --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
-    -t alexellis2/faas-watchdog:build .
-docker create --name buildoutput alexellis2/faas-watchdog:build echo
+    -t alexellis2/faas-watchdog:build-armhf . -f Dockerfile.armhf
+docker create --name buildoutput alexellis2/faas-watchdog:build-armhf echo
 docker cp buildoutput:/go/src/app/app ./fwatchdog
 docker rm buildoutput
+
