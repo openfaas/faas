d5290aad1bbaefb314c180a2a0c04963a09a4737
Alex Ellis <alexellis2@gmail.com>
2017-03-01 20:23:16 +0000

    Refactor to armv6

--- gateway/armhf.build.sh
+++ gateway/armhf.build.sh
@@ -8,6 +8,6 @@ docker create --name gateway_extract alexellis2/faas-gateway:build-armhf echo
 docker cp gateway_extract:/go/src/github.com/alexellis/faas/gateway/app ./gateway
 docker rm -f gateway_extract
 
-echo Building alexellis2/faas-gateway:latest-armhf
+echo Building alexellis2/faas-gateway:latest-armhf-dev
 
-docker build -t alexellis2/faas-gateway:latest-armhf .
+docker build -t alexellis2/faas-gateway:latest-armhf-dev .
