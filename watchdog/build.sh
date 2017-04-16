2618156414719c119286d9dc25e268be4e18bd17
Alex <alexellis2@gmail.com>
2017-04-16 17:01:22 -0500

    Revert Prometheus version

--- watchdog/build.sh
+++ watchdog/build.sh
@@ -3,8 +3,13 @@
 # Below makes use of "builder pattern" so that binary is extracted separate
 # from the golang runtime/SDK
 
-docker build --no-cache --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
-    -t functions/watchdog:build .
+if [ ! $http_proxy == "" ] 
+then
+    docker build --no-cache --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -t functions/watchdog:build .
+else
+    docker build -t functions/watchdog:build .
+fi
+
 docker create --name buildoutput functions/watchdog:build echo
 docker cp buildoutput:/go/src/github.com/alexellis/faas/watchdog/watchdog ./fwatchdog
 docker rm buildoutput
