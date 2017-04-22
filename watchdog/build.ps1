f952ad44049ed0255e96f096badb522003fb781a
Alex Ellis <alexellis2@gmail.com>
2017-04-22 16:29:16 -0500

    Create build.ps1

---
+++ watchdog/build.ps1
@@ -0,0 +1,3 @@
+docker build -t alexellis2/watchdog:windows . -f Dockerfile.win
+docker create --name watchdog alexellis2/watchdog:windows cmd
+docker cp watchdog:/go/src/github.com/alexellis2/watchdog/watchdog.exe .
