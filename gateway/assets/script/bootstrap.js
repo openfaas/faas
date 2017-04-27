569ccbf323e0ccca947f301bde40e2bb809c7ea7
Alex <alexellis2@gmail.com>
2017-04-27 22:47:09 +0100

    Add license into code files for MIT

--- gateway/assets/script/bootstrap.js
+++ gateway/assets/script/bootstrap.js
@@ -1,4 +1,7 @@
 "use strict"
+// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 var app = angular.module('faasGateway', ['ngMaterial']);
 
 app.controller("home", ['$scope', '$log', '$http', '$location', '$timeout', '$mdDialog', 
--- watchdog/types/types.go
+++ watchdog/types/types.go
@@ -1,3 +1,6 @@
+// Copyright (c) Alex Ellis 2017. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 package types
 
 import (
