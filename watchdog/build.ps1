556d02be1b9a4812a07b2712428109385591bb96
Alex Ellis <alexellis2@gmail.com>
2017-04-27 01:23:37 -0700

    Format for windows

--- watchdog/build.ps1
+++ watchdog/build.ps1
@@ -1,6 +1,10 @@
-pwd
 cd watchdog
 
+del watchdog.exe
+
 docker build -t alexellis2/watchdog:windows . -f .\Dockerfile.win
+
 docker create --name watchdog alexellis2/watchdog:windows cmd
-docker cp watchdog:/go/src/github.com/alexellis/faas/watchdog/watchdog.exe .
+
+& docker cp watchdog:/go/src/github.com/alexellis/faas/watchdog/watchdog.exe .
+docker rm -f watchdog
