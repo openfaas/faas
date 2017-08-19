cfcb183bc7db77b177d9552535e2f3101fd59e63
Alex Ellis <alexellis2@gmail.com>
2017-08-19 14:21:20 +0100

    Name builder steps
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gateway/build.sh
+++ gateway/build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-export TAG="latest-dev"
-echo Building functions/gateway:$TAG
+export eTAG="latest-dev"
+echo $1
+if [ $1 ] ; then
+  echo "set this"
+  eTAG=$1
+fi
+
+echo Building functions/gateway:$eTAG
 
 docker build --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
-  -t functions/gateway:$TAG .
+  -t functions/gateway:$eTAG .
+
