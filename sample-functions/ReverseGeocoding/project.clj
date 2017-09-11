0eb52a5cb5899ec1c848a2678a14a87e23bc7444
Luc Juggery <luc.juggery@gmail.com>
2017-09-11 20:07:04 +0200

    Cleanup project file
    Signed-off-by: Luc Juggery <luc.juggery@gmail.com>

--- sample-functions/ReverseGeocoding/project.clj
+++ sample-functions/ReverseGeocoding/project.clj
@@ -1,8 +1,5 @@
 (defproject reversegeocoding "0.1.0-SNAPSHOT"
   :description "Get address from GPS location"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [cheshire "5.5.0"]
