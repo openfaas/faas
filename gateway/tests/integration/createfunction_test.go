a3dd7a7db9379bc11de4ff0c43542c6f13f904a5
Alex Young <alex@heuris.io>
2017-09-19 22:33:41 +0100

    Add tests around error handing done in #196
    Signed-off-by: Alex Young <alex@heuris.io>

--- gateway/tests/integration/createfunction_test.go
+++ gateway/tests/integration/createfunction_test.go
@@ -4,13 +4,42 @@
 package inttests
 
 import (
+	"encoding/json"
 	"net/http"
+	"strings"
 	"testing"
 )
 
-func TestCreate_ValidJson(t *testing.T) {
-	reqBody := `{}`
-	_, code, err := fireRequest("http://localhost:8080/system/functions", http.MethodPost, reqBody)
+type PostFunctionRequest struct {
+	Image      string `json:"image"`
+	EnvProcess string `json:"envProcess"`
+	Network    string `json:"network"`
+	Service    string `json:"service"`
+}
+
+type DeleteFunctionRequest struct {
+	FunctionName string `json:"functionName"`
+}
+
+func createFunction(request PostFunctionRequest) (string, int, error) {
+	marshalled, _ := json.Marshal(request)
+	return fireRequest("http://localhost:8080/system/functions", http.MethodPost, string(marshalled))
+}
+
+func deleteFunction(name string) (string, int, error) {
+	marshalled, _ := json.Marshal(DeleteFunctionRequest{name})
+	return fireRequest("http://localhost:8080/system/functions", http.MethodDelete, string(marshalled))
+}
+
+func TestCreate_ValidRequest(t *testing.T) {
+	request := PostFunctionRequest{
+		"functions/resizer",
+		"",
+		"func_functions",
+		"test_resizer",
+	}
+
+	_, code, err := createFunction(request)
 
 	if err != nil {
 		t.Log(err)
@@ -18,11 +47,70 @@ func TestCreate_ValidJson(t *testing.T) {
 	}
 
 	if code != http.StatusOK {
-		t.Errorf("Got HTTP code: %d, want %d\n", code, http.StatusBadRequest)
+		t.Errorf("Got HTTP code: %d, want %d\n", code, http.StatusOK)
+		return
+	}
+
+	deleteFunction("test_resizer")
+}
+
+func TestCreate_InvalidImage(t *testing.T) {
+	request := PostFunctionRequest{
+		"a b c",
+		"",
+		"func_functions",
+		"test_resizer",
+	}
+
+	body, code, err := createFunction(request)
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	expectedErrorCode := http.StatusBadRequest
+	if code != expectedErrorCode {
+		t.Errorf("Got HTTP code: %d, want %d\n", code, expectedErrorCode)
+		return
+	}
+
+	expectedErrorSlice := "is not a valid repository/tag"
+	if !strings.Contains(body, expectedErrorSlice) {
+		t.Errorf("Error message %s does not contain: %s\n", body, expectedErrorSlice)
+		return
 	}
 }
 
-func TestCreateBadFunctionNotJson(t *testing.T) {
+func TestCreate_InvalidNetwork(t *testing.T) {
+	request := PostFunctionRequest{
+		"functions/resizer",
+		"",
+		"non_existent_network",
+		"test_resizer",
+	}
+
+	body, code, err := createFunction(request)
+
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	expectedErrorCode := http.StatusBadRequest
+	if code != expectedErrorCode {
+		t.Errorf("Got HTTP code: %d, want %d\n", code, expectedErrorCode)
+		return
+	}
+
+	expectedErrorSlice := "network non_existent_network not found"
+	if !strings.Contains(body, expectedErrorSlice) {
+		t.Errorf("Error message %s does not contain: %s\n", body, expectedErrorSlice)
+		return
+	}
+}
+
+func TestCreate_InvalidJson(t *testing.T) {
 	reqBody := `not json`
 	_, code, err := fireRequest("http://localhost:8080/system/functions", http.MethodPost, reqBody)
 
