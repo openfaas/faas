5af0eda09afc05314a84b5cd38831fa4cfeebe4e
Alex Ellis <alexellis2@gmail.com>
2017-09-25 19:42:37 +0100

    Update ci.sh

--- contrib/ci.sh
+++ contrib/ci.sh
@@ -4,9 +4,9 @@ docker swarm init --advertise-addr=$(hostname -i)
 
 ./deploy_stack.sh
 
-cd gateway/tests/integration
-go test -v
-cd ../../../
+#cd gateway/tests/integration
+#go test -v
+#cd ../../../
 
 git clone https://github.com/openfaas/certify-incubator
 cd certify-incubator && \
