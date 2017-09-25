d5decf4ebf1ec58024df69d6f06d9a525e5038a9
Alex Ellis <alexellis2@gmail.com>
2017-09-25 21:17:35 +0100

    Update ci.sh

--- contrib/ci.sh
+++ contrib/ci.sh
@@ -8,17 +8,17 @@ cd ..
 
 echo $GOPATH
 
-mkdir -p $GOPATH/go/src/github.com/openfaas/
-cp -r faas $GOPATH/go/src/github.com/openfaas/
+mkdir -p $GOPATH/src/github.com/openfaas/
+cp -r faas $GOPATH/src/github.com/openfaas/
 
 git clone https://github.com/openfaas/certify-incubator
 
-cp -r certify-incubator $GOPATH/go/src/github.com/openfaas/
+cp -r certify-incubator $GOPATH/src/github.com/openfaas/
 
-cd $GOPATH/go/src/github.com/openfaas/faas/ && \
+cd $GOPATH/src/github.com/openfaas/faas/gateway/tests/integration && \
    go test -v
 
-cd $GOPATH/go/src/github.com/openfaas/certify-incubator && \
+cd $GOPATH/src/github.com/openfaas/certify-incubator && \
    make test
 
 exit 0
