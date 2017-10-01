f2d665c5462c2f26be84abcd53fa263d68c8874f
Alex Ellis <alexellis2@gmail.com>
2017-10-01 13:29:37 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [[ "$TRAVIS_TAG" != null ]]; then
+    echo "This build will be published under the tag ${TRAVIS_TAG}"
+fi
+
 (cd gateway && ./build.sh)
 (cd watchdog && ./build.sh)
