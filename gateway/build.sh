80c9092522d25574444c44ba53f9ee74c3ce1e65
Alex Ellis <alexellis2@gmail.com>
2017-10-01 19:57:39 +0000

    Update for ARM builds
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gateway/build.sh
+++ gateway/build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+export dockerfile="Dockerfile"
+export arch=$(uname -m)
+
 export eTAG="latest-dev"
+
+if [ "$arch" = "armv7l" ] ; then
+   dockerfile="Dockerfile.armhf"
+   eTAG="latest-armhf-dev"
+fi
+
 echo "$1"
 if [ "$1" ] ; then
   eTAG=$1
@@ -9,5 +18,6 @@ fi
 echo Building functions/gateway:$eTAG
 
 docker build --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
-  -t functions/gateway:$eTAG .
+  -t functions/gateway:$eTAG . -f $dockerfile
+
 
