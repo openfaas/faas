4e848afec6baf13cf5a1788944c6e40d2e8b9b6a
Alex Ellis <alexellis2@gmail.com>
2017-10-01 13:33:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_TAG" != null ]]; then
-    echo "This build will be published under the tag ${TRAVIS_TAG}"
+if [ ! -s $TRAVIS_TAG ] ; then
+    echo "This build will be published under the tag: ${TRAVIS_TAG}"
 fi
 
 (cd gateway && ./build.sh)
