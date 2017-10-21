77b4c06f054e66c4289a7ebf8f7d559aa641785d
rgee0 <richard@technologee.co.uk>
2017-10-21 13:29:50 +0100

    Consolidated build and deploy scripts fixes #249
    Signed-off-by: rgee0 <richard@technologee.co.uk>

--- gateway/build.sh
+++ gateway/build.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-export dockerfile="Dockerfile"
-export arch=$(uname -m)
+export ARCH=$(uname -m)
 
-export eTAG="latest-dev"
+case "$ARCH" in
 
-if [ "$arch" = "armv7l" ] ; then
-   dockerfile="Dockerfile.armhf"
-   eTAG="latest-armhf-dev"
-fi
+"x86_64" )              DOCKERFILE="Dockerfile"
+                        eTAG="latest-dev"
+                        ;;
+
+"armv7l" )              DOCKERFILE="Dockerfile.armhf"
+                        eTAG="latest-armhf-dev"
+                        ;;
+
+"armv8l" | "aarch64" )  DOCKERFILE="Dockerfile.arm64"
+                        eTAG="latest-arm64-dev"
+                        ;;
+
+*) echo "Sorry, your architecture ($ARCH) cannot currently be matched to a build Dockerfile.  Please raise an issue at https://github.com/openfaas/faas"
+   exit 1
+   ;;
+esac
 
 echo "$1"
 if [ "$1" ] ; then
@@ -18,6 +29,6 @@ fi
 echo Building functions/gateway:$eTAG
 
 docker build --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy \
-  -t functions/gateway:$eTAG . -f $dockerfile
+  -t functions/gateway:$eTAG . -f $DOCKERFILE
 
 
