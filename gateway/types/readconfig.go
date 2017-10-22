06f0a2c953eab24335a80fb3b47a606d7ca86b83
Elliott Beach <elliott2.71828@gmail.com>
2017-10-22 00:17:18 -0500

    add docs for GatewayConfig like WatchdogConfig
    Signed-off-by: Elliott Beach <elliott2.71828@gmail.com>

--- gateway/types/readconfig.go
+++ gateway/types/readconfig.go
@@ -102,12 +102,26 @@ func (ReadConfig) Read(hasEnv HasEnv) GatewayConfig {
 
 // GatewayConfig for the process.
 type GatewayConfig struct {
+
+	// HTTP timeout for reading a request from clients.
 	ReadTimeout          time.Duration
+
+	// HTTP timeout for writing a response from functions.
 	WriteTimeout         time.Duration
+
+	// URL for alternate functions provider.
 	FunctionsProviderURL *url.URL
+
+	// Address of the NATS service. Required for async mode.
 	NATSAddress          *string
+
+	// Port of the NATS Service. Required for async mode.
 	NATSPort             *int
+
+	// Host to connect to Prometheus.
 	PrometheusHost       string
+
+	// Port to connect to Prometheus.
 	PrometheusPort       int
 }
 
