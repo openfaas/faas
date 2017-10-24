15d09a00e62bc92357da6c2ab349c9204d57e5f7
mlabouardy <mohamed@labouardy.com>
2017-10-24 10:21:21 +0200

    update package name

--- sample-functions/Memes9Gag/handler.go
+++ sample-functions/Memes9Gag/handler.go
@@ -1,20 +1,13 @@
-package main
+package function
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 
 	"github.com/mlabouardy/9gag"
 )
 
-func main() {
-	tag, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("Unable to read standard input: %s", err.Error())
-	}
+func Handle(req []byte) string {
 	gag9 := gag9.New()
-	memes := gag9.FindByTag(string(tag))
-	fmt.Println(memes)
+	memes := gag9.FindByTag(string(req))
+	return fmt.Sprintf("%s", memes)
 }
