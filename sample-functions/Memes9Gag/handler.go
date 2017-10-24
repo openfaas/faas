e1703c89554a0fdf84f901410a53f0fdd3adf5d0
mlabouardy <mohamed@labouardy.com>
2017-10-24 10:50:44 +0200

    fix handler.go

--- sample-functions/Memes9Gag/handler.go
+++ sample-functions/Memes9Gag/handler.go
@@ -1,13 +1,20 @@
-package function
+package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/mlabouardy/9gag"
 )
 
-func Handle(req []byte) string {
+func main() {
+	tag, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("Unable to read standard input: %s", err.Error())
+	}
 	gag9 := gag9.New()
-	memes := gag9.FindByTag(string(req))
-	return fmt.Sprintf("%s", memes)
+	memes := gag9.FindByTag(string(tag))
+	fmt.Println(memes)
 }
