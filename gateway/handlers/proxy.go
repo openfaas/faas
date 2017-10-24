7a29ce2337b8d2db4a0b0a8252cd9c6e703551e6
Jock Reed <jock.da.rock@gmail.com>
2017-10-23 23:00:29 -0500

    proxy fix to pass GET method along to function as an environment variable
    Signed-off-by: JockDaRock <jock.da.rock@gmail.com>

--- gateway/handlers/proxy.go
+++ gateway/handlers/proxy.go
@@ -141,7 +141,8 @@ func invokeService(w http.ResponseWriter, r *http.Request, metrics metrics.Metri
 	contentType := r.Header.Get("Content-Type")
 	fmt.Printf("[%s] Forwarding request [%s] to: %s\n", stamp, contentType, url)
 
-	request, err := http.NewRequest("POST", url, bytes.NewReader(requestBody))
+	method := r.Method
+	request, err := http.NewRequest(method, url, bytes.NewReader(requestBody))
 
 	copyHeaders(&request.Header, &r.Header)
 
