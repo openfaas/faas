ec60d975188ecd843d82a640471388e9ee3c149f
Alex Ellis <alexellis2@gmail.com>
2017-10-25 19:30:30 +0100

    Report async execution duration
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gateway/handlers/asyncreport.go
+++ gateway/handlers/asyncreport.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/openfaas/faas/gateway/metrics"
 	"github.com/openfaas/faas/gateway/requests"
@@ -22,5 +23,9 @@ func MakeAsyncReport(metrics metrics.MetricOptions) http.HandlerFunc {
 		json.Unmarshal(bytesOut, &report)
 
 		trackInvocation(report.FunctionName, metrics, report.StatusCode)
+
+		var taken time.Duration
+		taken = time.Duration(report.TimeTaken)
+		trackTimeExact(taken, metrics, report.FunctionName)
 	}
 }
