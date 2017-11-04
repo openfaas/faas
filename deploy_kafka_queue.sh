8a2a036a82fbfd971b47cd483624b3f3d7c8e0fe
Yigong Liu <ygl.code@gmail.com>
2017-11-04 12:41:08 -0700

    update deploy script and docker-compose for kakfka-queue-worker
    Signed-off-by: Yigong Liu <ygl.code@gmail.com>

--- deploy_kafka_queue.sh
+++ deploy_kafka_queue.sh
@@ -5,5 +5,9 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 1
 fi
 
+echo "Create overlay network "functions" for faas"
+docker network create functions -d overlay --attachable 
+
 echo "Deploying extended stack with kafka queue"
+docker stack deploy kfk --compose-file docker-compose.kafka-setup.yml
 docker stack deploy func --compose-file docker-compose.kafka-queue.yml
