117197dbd005cfbbcc22dafad14a952f2e844d25
Burton Rheutan <rheutan7@gmail.com>
2017-11-12 00:49:02 -0600

    Updated the NodeJS sample functions to use the latest LTS versions. Fixes #383
    Signed-off-by: Burton Rheutan <rheutan7@gmail.com>

--- sample-functions/BaseFunctions/node/build.sh
+++ sample-functions/BaseFunctions/node/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Building functions/base:node-6.9.1-alpine"
-docker build -t functions/base:node-6.9.1-alpine .
+echo "Building functions/base:node-8.9.1-alpine"
+docker build -t functions/base:node-8.9.1-alpine .
 
