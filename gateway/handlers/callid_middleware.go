cd4c5468df0714ade08a6d2569ad26a36ec274e3
Alex Ellis <alexellis2@gmail.com>
2017-12-13 14:14:55 +0000

    Don't override X-Call-Id if it already exists in header
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gateway/handlers/callid_middleware.go
+++ gateway/handlers/callid_middleware.go
@@ -12,9 +12,11 @@ import (
 func MakeCallIDMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		callID := uuid.Generate().String()
-		r.Header.Add("X-Call-Id", callID)
-		w.Header().Add("X-Call-Id", callID)
+		if len(r.Header.Get("X-Call-Id")) == 0 {
+			callID := uuid.Generate().String()
+			r.Header.Add("X-Call-Id", callID)
+			w.Header().Add("X-Call-Id", callID)
+		}
 
 		r.Header.Add("X-Start-Time", fmt.Sprintf("%d", start.UTC().UnixNano()))
 		w.Header().Add("X-Start-Time", fmt.Sprintf("%d", start.UTC().UnixNano()))
