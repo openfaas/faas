34fee67be3fbaf323c262ac0585f89e4a089cc5e
Tommy Stigen Olsen <tommysolsen@gmail.com>
2018-01-03 16:34:51 +0100

    Use default prometheus/alertmanager containers
    This commit removes the old custom docker containers for different
    versions of the alertmanager and prometheus.
    
    Signed-off-by: Tommy Stigen Olsen <tommysolsen@gmail.com>

---
+++ deploy_stack.k8s.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Unable to find docker command, please install Docker (https://www.docker.com/) and retry' >&2
+  exit 1
+fi
+
+echo "Deploying stack"
+docker stack deploy func --compose-file k8s.docker-compose.yml
+
