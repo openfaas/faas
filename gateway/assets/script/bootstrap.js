1713fd4cbbd2d82976cbb8602c0b8633d0bc4d51
Ken Fukuyama <kenfdev@gmail.com>
2018-02-27 23:24:19 +0900

    fixed ES6 syntax accidentally used in JavaScript. fixed markup closing typo.
    Signed-off-by: Ken Fukuyama <kenfdev@gmail.com>

--- gateway/assets/script/bootstrap.js
+++ gateway/assets/script/bootstrap.js
@@ -294,8 +294,8 @@ app.controller("home", ['$scope', '$log', '$http', '$location', '$timeout', '$md
 ]);
 
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, function(c) {
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    )
+    })
   }
   
