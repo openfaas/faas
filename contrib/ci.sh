034f2c4d13b8fb6cdae339ddf66c0e3b9fde3f0a
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-03-29 09:58:50 +0100

    Update travis to use Go 1.9
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- contrib/ci.sh
+++ contrib/ci.sh
@@ -3,6 +3,20 @@
 docker swarm init --advertise-addr=127.0.0.1
 
 ./deploy_stack.sh
+docker service update func_gateway --image=functions/gateway:latest-dev
+
+# Script makes sure OpenFaaS API gateway is ready before running tests
+
+for i in {1..30};
+do
+  echo "Checking if 127.0.0.1:8000 is up.. ${i}/30" 
+  curl -fs 127.0.0.1:8080/
+
+  if [ $? -eq 0 ]; then
+    break
+  fi
+  sleep 0.5
+done
 
 cd ..
 
@@ -11,14 +25,14 @@ echo $GOPATH
 mkdir -p $GOPATH/src/github.com/openfaas/
 cp -r faas $GOPATH/src/github.com/openfaas/
 
-git clone https://github.com/openfaas/certify-incubator
+git clone https://github.com/openfaas/certifier
 
-cp -r certify-incubator $GOPATH/src/github.com/openfaas/
+cp -r certifier $GOPATH/src/github.com/openfaas/
 
 cd $GOPATH/src/github.com/openfaas/faas/gateway/tests/integration && \
    go test -v
 
-cd $GOPATH/src/github.com/openfaas/certify-incubator && \
+cd $GOPATH/src/github.com/openfaas/certifier && \
    make test
 
 exit 0
