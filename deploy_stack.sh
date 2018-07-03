d12482ed681b3bcb603f4d2b726ad572ccf0a382
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-07-03 10:41:58 +0100

    Enable basic-auth by default
    The deployment script will enable basic-auth by default to help
    avoid people deploying to a public IP with no protection from
    malicious actors.
    
    Tested on MacOS with and without --no-auth flag.
    
    Does not apply for armhf or powershell.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -5,6 +5,46 @@ if ! [ -x "$(command -v docker)" ]; then
   exit 1
 fi
 
-echo "Deploying stack"
-docker stack deploy func --compose-file docker-compose.yml
+auth="1"
 
+while [ ! $# -eq 0 ]
+do
+	case "$1" in
+		--no-auth | -n)
+			auth=0
+			;;
+    --help | -h)
+			echo "Usage: \n [default]\tdeploy the OpenFaaS core services\n --no-auth [-n]\tdisable basic authentication.\n --help\tdisplays this screen"
+      exit
+			;;
+	esac
+	shift
+done
+
+# Secrets should be created even if basic-auth is disabled.
+echo "Attempting to create credentials for gateway.."
+echo "admin" | docker secret create basic-auth-user -
+secret=$(head -c 16 /dev/random|shasum -a 256 | cut -d " " -f 1)
+echo "$secret" | docker secret create basic-auth-password -
+if [ $? = 0 ];
+then
+  echo "[Credentials]\n username: admin \n password: $secret\n echo -n "$secret" | faas-cli login --username=admin --password-stdin"
+else
+  echo "[Credentials]\n already exist, not creating"
+fi
+
+if [ $auth -eq "1" ];
+then
+  echo ""
+  echo "Enabling basic authentication for gateway.."
+  echo ""
+else
+  echo ""
+  echo "Disabling basic authentication for gateway.."
+  echo ""
+  sed -i -r.bak 's/basic_auth: \"true\"/basic_auth: \"false\"/' docker-compose.yml
+fi
+
+echo "Deploying OpenFaaS core services"
+
+docker stack deploy func --compose-file docker-compose.yml
