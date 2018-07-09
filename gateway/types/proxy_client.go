5f6b6bf95377d877fb1e774107c211636be21d73
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-07-09 21:25:03 +0100

    Update header
    From Project to Author(s) for the catch-all. This file has my
    copyright which is retained through the catch-all statement.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- gateway/types/proxy_client.go
+++ gateway/types/proxy_client.go
@@ -1,4 +1,4 @@
-// Copyright (c) OpenFaaS Project 2018. All rights reserved.
+// Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 package types
