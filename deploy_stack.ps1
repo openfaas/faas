81ce583bf03fdaffaae668703cc09377cd6c6d09
John McCabe <john@johnmccabe.net>
2018-10-20 03:39:01 +0100

    Prompt for user/password in deploy_stack.ps1
    This commit updates the Windows deploy_stack.ps1 to set the
    basic-auth-user and basic-auth-password secrets if absent.
    
    Signed-off-by: John McCabe <john@johnmccabe.net>

--- deploy_stack.ps1
+++ deploy_stack.ps1
@@ -2,6 +2,31 @@
 
 if (Get-Command docker -errorAction SilentlyContinue)
 {
+
+    $user_secret = "basic-auth-user"
+    docker secret inspect $user_secret 2>&1 | out-null
+    if($?)
+    {
+        Write-Host "$user_secret secret exists"
+    }
+    else
+    {
+        $user = Read-Host 'Admin User?'
+        $user | docker secret create $user_secret -
+    }
+
+    $password_secret = "basic-auth-password"
+    docker secret inspect $password_secret 2>&1 | out-null
+    if($?)
+    {
+        Write-Host "$password_secret secret exists"
+    }
+    else
+    {
+        $pass = Read-Host 'Password?' -AsSecureString
+        [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass)) | docker secret create $password_secret -
+    }
+
     Write-Host "Deploying stack"
     docker stack deploy func --compose-file ./docker-compose.yml
 }
