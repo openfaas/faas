fca32a0e79af87c6c07ee6281042ca2ec64ca297
Alex Ellis (VMware) <alexellis2@gmail.com>
2019-01-05 10:17:18 +0000

    Instrument async handlers
    - instruments async handler for report and for queueing async
    requests
    - make MustRegister only ever run once to prevent sync issues
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- gateway/metrics/exporter_test.go
+++ gateway/metrics/exporter_test.go
@@ -40,24 +40,27 @@ func Test_Describe_DescribesThePrometheusMetrics(t *testing.T) {
 
 	go exporter.Describe(ch)
 
-	d := (<-ch)
+	d := <-ch
 	expectedGatewayFunctionInvocationDesc := `Desc{fqName: "gateway_function_invocation_total", help: "Individual function metrics", constLabels: {}, variableLabels: [function_name code]}`
 	actualGatewayFunctionInvocationDesc := d.String()
 	if expectedGatewayFunctionInvocationDesc != actualGatewayFunctionInvocationDesc {
 		t.Errorf("Want %s, got: %s", expectedGatewayFunctionInvocationDesc, actualGatewayFunctionInvocationDesc)
 	}
-	d = (<-ch)
+
+	d = <-ch
 	expectedGatewayFunctionsHistogramDesc := `Desc{fqName: "gateway_functions_seconds", help: "Function time taken", constLabels: {}, variableLabels: [function_name]}`
 	actualGatewayFunctionsHistogramDesc := d.String()
 	if expectedGatewayFunctionsHistogramDesc != actualGatewayFunctionsHistogramDesc {
 		t.Errorf("Want %s, got: %s", expectedGatewayFunctionsHistogramDesc, actualGatewayFunctionsHistogramDesc)
 	}
-	d = (<-ch)
+
+	d = <-ch
 	expectedServiceReplicasGaugeDesc := `Desc{fqName: "gateway_service_count", help: "Docker service replicas", constLabels: {}, variableLabels: [function_name]}`
 	actualServiceReplicasGaugeDesc := d.String()
 	if expectedServiceReplicasGaugeDesc != actualServiceReplicasGaugeDesc {
 		t.Errorf("Want %s, got: %s", expectedServiceReplicasGaugeDesc, actualServiceReplicasGaugeDesc)
 	}
+
 }
 
 func Test_Collect_CollectsTheNumberOfReplicasOfAService(t *testing.T) {
