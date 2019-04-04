947797092465ef167941567e7cdb007ca5690aaf
Matias Pan <matias.pan26@gmail.com>
2019-04-04 09:57:28 -0300

    Add Http_Content_Length to env variables
    Http_ContentLengh will be deprecated in the future.
    
    Signed-off-by: Matias Pan <matias.pan26@gmail.com>

--- watchdog/handler.go
+++ watchdog/handler.go
@@ -228,7 +228,9 @@ func getAdditionalEnvs(config *WatchdogConfig, r *http.Request, method string) [
 		}
 
 		envs = append(envs, fmt.Sprintf("Http_Method=%s", method))
+		// DEPRECATED: Http_ContentLength will be deprecated in favour
 		envs = append(envs, fmt.Sprintf("Http_ContentLength=%d", r.ContentLength))
+		envs = append(envs, fmt.Sprintf("Http_Content_Length=%d", r.ContentLength))
 
 		if config.writeDebug {
 			log.Println("Query ", r.URL.RawQuery)
