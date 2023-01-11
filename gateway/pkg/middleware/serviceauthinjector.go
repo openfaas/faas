fbc0ebdf4a52b2cd7c23555cf47925b1918b25fb
Alex Ellis (OpenFaaS Ltd) <alex@openfaas.com>
2023-01-11 13:10:46 +0000

    Add comment to middleware
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alex@openfaas.com>

--- gateway/pkg/middleware/serviceauthinjector.go
+++ gateway/pkg/middleware/serviceauthinjector.go
@@ -2,6 +2,8 @@ package middleware
 
 import "net/http"
 
+// AuthInjector is an interface for injecting authentication information into a request
+// which will be proxied or made to a remote/upstream service.
 type AuthInjector interface {
 	Inject(r *http.Request)
 }
